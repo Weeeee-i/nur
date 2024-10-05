59ff0612bbfb290cb7a8927b861efd0b7651e8f1
Weeeee-i <weeeeei@duck.com>
2024-10-05 14:56:08 +0800

    Fix Hash

--- pkgs/sys-montion/default.nix
+++ pkgs/sys-montion/default.nix
@@ -10,16 +10,16 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "Weeeee-i";
     repo = pname;
-    rev = version;
-    hash = "";
+    rev = "b8fe480ebfd064d7af5efbbb34f068b310f77369";
+    hash = "sha256-zF2Tl9ov59hBUjzh/cnJ3Hewew+icOw5rbHSONvvS2U=";
   };
 
-  cargoHash = "";
+  cargoHash = "sha256-Tk47bSqJ0oifNHtbOstznncML6Lb0jUVbY4pFfhpzqI=";
 
   meta = with lib; {
     description = "A system monitor";
     homepage = "https://github.com/Weeeee-i/sys-montion";
-    license = license.unlicense;
+    # license = license.unlicense;
     maintainers = [];
   };
 }
